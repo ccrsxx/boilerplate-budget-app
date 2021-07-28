45fedccd870df129677f28e061f2f2168547982c
ccrsxx <55032197+ccrsxx@users.noreply.github.com>
2021-07-29 00:24:38 +0700

    update

--- budget.py
+++ budget.py
@@ -25,6 +25,7 @@ class Category:
             self.ledger.append({"amount": -amount, "description": description})
             return True
         else:
+            print(f'Not enough money to buy {description}')
             return False
 
     def get_balance(self):
@@ -36,6 +37,7 @@ class Category:
             category.deposit(amount, f'Transfer from {self.category}')
             return True
         else:
+            print(f'Not enough money to transfer to {category.category}')
             return False
 
     def __str__(self):
@@ -46,28 +48,47 @@ class Category:
             amount = f'{item["amount"]:.2f}'[:7]
             items += f'{desc:<23}{amount:>7}\n'
         total = f'{self.get_balance():.2f}'[:7]
-        return f'{title}{items}Total: {total}'
+        object = f'{title}{items}Total: {total}'
+        return object
+
+def truncate(num, decimals=0):
+    multiplier = 10 ** decimals
+    return int(num * multiplier) / multiplier
 
 def spent_percentage(categories):
     all_percentage = {}
     all_spent = sum([cat.spent for cat in categories])
     for cat in categories:
-        val = round_ten(cat.spent / all_spent * 100)
+        val = truncate((cat.spent / all_spent * 100), -1)
         all_percentage[cat.category] = val
     return all_percentage
 
-def round_ten(num):
-    if num % 10 < 5:
-        return int(num / 10) * 10
-    else:
-        return int((num + 10) / 10) * 10
-
 def create_spend_chart(categories):
+    mid = first_mid = end_mid =''
+    down = first_down = end_down = ''
+    all_percentage = spent_percentage(categories)
     up = 'Percentage spent by category\n'
-    
-
-
-
-
-
-    return up
+    for i in reversed(range(0, 101, 10)):
+        first_mid = f'{i:>3}|'
+        end_mid = ''
+        for cat in all_percentage:
+            if all_percentage[cat] >= i:
+                val = 'o'
+            else:
+                val = ''
+            end_mid += f'{val:^3}'
+        mid += (f'{first_mid}{end_mid}\n')
+    mid_line = f'{"":4}{"-" * (len(categories) * 3 + 1)}\n'
+    words_array = [i.split() for i in all_percentage]
+    max_word = max([len(i) for i in all_percentage])
+    for i in range(max_word):
+        end_down = ''
+        for word in words_array:
+            if len(word[0]) > i:
+                val = word[0][i]
+            else:
+                val = ''
+            end_down += f'{val:^3}'
+        down += f'{"":4}{first_down}{end_down}\n'
+    chart = f'{up}{mid}{mid_line}{down}'
+    return chart
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,16 +1,3 @@
-food = Category("Food")
-# food.deposit(1000, "initial deposit")
-# food.withdraw(10.15, "groceries")
-# food.withdraw(15.89, "restaurant and more food for dessert")
-# print(food.get_balance())
-# clothing = Category("Clothing")
-# food.transfer(50, clothing)
-# clothing.withdraw(25.55)
-# clothing.withdraw(100)
-# auto = Category("Auto")
-# auto.deposit(1000, "initial deposit")
-# auto.withdraw(15)
-
-# print(food)
-# print(clothing)
-# print(auto)
+    # for name in all_percentage:
+    #     for word in name:
+    #         names = 
