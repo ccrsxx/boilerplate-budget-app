a83ad401a0fae0c512f862f951eae6d58f6f2b3c
ccrsxx <55032197+ccrsxx@users.noreply.github.com>
2021-07-28 11:05:11 +0700

    adding multiple deposite ability

--- budget.py
+++ budget.py
@@ -3,12 +3,13 @@ class Category:
     def __init__(self, category):
         self.category = category
         self.ledger = []
-        print(self.category, 'created')
 
     def deposit(self, amount, description=''):
+        if not self.ledger:
+            self.balance = amount
+        else:
+            self.balance += self.ledger[-1]['amount']
         self.ledger.append({"amount": amount, "description": description})
-        self.balance = self.ledger[0]['amount']
-        print(f'{self.ledger[0]["amount"]}$ deposited {self.ledger[0]["description"]!r}')
 
     def check_funds(self, amount):
         if self.balance >= amount:
@@ -20,10 +21,8 @@ class Category:
         if self.check_funds(amount):
             self.balance -= amount
             self.ledger.append({"amount": -amount, "description": description})
-            print(f'{"%.2f" % self.ledger[0]["amount"]}$ left -{amount}$ {description!r}')
             return True
         else:
-            print(f'Not enough money to buy {description}')
             return False
 
     def get_balance(self):
@@ -49,34 +48,6 @@ class Category:
         return f'{title}{items}Total: {total}'
 
 
-
-
-food = Category("Food")
-food.deposit(1000, "initial deposit")
-food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
-print(food.get_balance())
-clothing = Category("Clothing")
-food.transfer(50, clothing)
-clothing.withdraw(25.55)
-clothing.withdraw(100)
-auto = Category("Auto")
-auto.deposit(1000, "initial deposit")
-auto.withdraw(15)
-
-emilia = Category('Emilia')
-emilia.deposit(200, 'by emilia')
-emilia.withdraw(69.69, 'buying keyboard')
-emilia.withdraw(20, 'buying novel')
-emilia.withdraw(25, 'get a vaccine')
-emilia.get_balance()
-
-rem = Category('Rem')
-emilia.transfer(60, rem)
-
-emilia.get_balance()
-rem.get_balance()
-
-print(clothing)
-print(auto)
-print(food)
+def create_spend_chart(categories):
+    create_spend_chart = None
+    return create_spend_chart
